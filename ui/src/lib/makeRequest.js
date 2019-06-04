e9ac8cb9d0df2fe3c956986ddb1f71077867998c
Akeem King <akeemtlking@gmail.com>
2019-06-03 21:25:33 -0400

    Adding documentation

--- ui/src/lib/makeRequest.js
+++ ui/src/lib/makeRequest.js
@@ -1,3 +1,10 @@
+/**
+ * This function provides a simple interface to perform a HTTP request.
+ *
+ * @param {string} method - the type of request to perform. e.g. GET, POST, PUT, etc.
+ * @param {string} url - the url of the resource.
+ * @param {Object} data - data to be sent as body
+ */
 export default function (method, url, data) {
     return new Promise(function (resolve, reject) {
             const xhr = new XMLHttpRequest();
