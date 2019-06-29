5dcab5883b1a1362169384c66f87f694085cf0df
Akeem King <akeemtlking@gmail.com>
2019-06-29 14:58:42 -0400

    Removing dev folder

--- dev/run_gui.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-cd ../ui
-yarn run start
-
--- dev/run_server.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-export TOR_ADDRESS=127.0.0.1
-export TOR_PORT=9050
-export GET_LINKS_CONCURRENCY=5 # Determines maximum number of concurrent goroutines that can be run with get links operation.
-
-cd ../server/
-go run main.go
-
